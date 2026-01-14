22f175d18a59ca8c8fe04316117d756429d70815
madhubolton <madhu@meltingpointmedia.com>
2026-01-14 10:12:55 -0800

    Update ah-oracle-scene-v3.js

--- ah-oracle-scene-v3.js
+++ ah-oracle-scene-v3.js
@@ -36,7 +36,7 @@ function getResponsiveScales() {
     return {
       metatronScale: 1.4,
       goddessDockScale: 1.5,
-      goddessCenterScale: 2.0,
+      goddessCenterScale: 1.0,
       goddessDockY: '5vh'
     };
   }
