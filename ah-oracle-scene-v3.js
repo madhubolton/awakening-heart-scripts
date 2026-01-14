345de4a3d8694078101943a20fd58c3e9c7e35c4
madhubolton <madhu@meltingpointmedia.com>
2026-01-14 10:24:11 -0800

    Update ah-oracle-scene-v3.js

--- ah-oracle-scene-v3.js
+++ ah-oracle-scene-v3.js
@@ -34,7 +34,7 @@ function getResponsiveScales() {
   if (width <= 480) {
     console.log('📱 Responsive: Phone portrait mode');
     return {
-      metatronScale: 0.95,
+      metatronScale: 1.0,
       goddessDockScale: 1.5,
       goddessCenterScale: 2.0,
       goddessDockY: '12vh'
